842770cf1df763035482bab56f57f19aa2af29e2
ravibandapalli <ravi.bat456@example.com>
2025-11-01 23:37:11 +0530

    Fix: corrected crossOrigin attribute for font preconnect

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
       <head>
         {/* Load Google Fonts manually (no next/font) */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
           rel="stylesheet"
